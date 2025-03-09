5eaf95664cb78d14a529a26979cb116d26b190d7
dev-vhungitm <dev.vhungitm@gmail.com>
2025-03-09 14:34:17 +0700

    .git

--- configs.ts
+++ configs.ts
@@ -1,4 +1,4 @@
-const imageValidTypes = 'image/webp,image/jpeg,image/png,image/svg+xml,image/gif,image/bmp';
+const imageValidTypes = 'image/webp,image/jpeg,image/png,image/svg+xml,image/gif,image/bmp,image/tiff';
 const imageValidTypesPattern = /^image\/(webp|jpeg|jpg|png|svg|gif|bmp|tiff)$/;
 const dateFormats = {
 	vnDate: 'DD/MM/YYYY',
